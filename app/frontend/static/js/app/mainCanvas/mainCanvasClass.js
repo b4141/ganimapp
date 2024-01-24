38d8450feb4f12ebe4c629dbe1116866aee4ec91
b4141 <barikmail1@gmail.com>
2024-01-24 14:53:46 +0100

    reimplantation of the background drawing using css

--- app/frontend/static/js/app/mainCanvas/mainCanvasClass.js
+++ app/frontend/static/js/app/mainCanvas/mainCanvasClass.js
@@ -11,8 +11,6 @@ export default class MainCanvas {
     this.objects_on_canvas = [];
     this.selected_objects = [];
     this.drawSelectAreaInfo = { state: false, startXPos: 0, startYPos: 0 };
-
-    this.loadBackground("./static/imgs/png/gridImage.png");
   }
 
   //___TODO___modify_this_function_its_only_a_place_holder
@@ -27,29 +25,8 @@ export default class MainCanvas {
     }
   }
 
-  loadBackground(src) {
-    this.backgroundImage = new Image();
-    this.backgroundImage.src = src;
-    this.backgroundImage.onload = () => {
-      this.update();
-    }
-  }
-
-  drawBackground() {
-    // this.ctx.drawImage(this.backgroundImage, 0 + this.origin, 0 + this.origin);
-    // console.log(this.canvasElement.width)
-    for (let i = 0; i < Math.floor(this.canvasElement.width / this.backgroundImage.width) + 1; i++) {
-      for (let j = 0; j < Math.floor(this.canvasElement.height / this.backgroundImage.height) + 1; j++) {
-        this.ctx.drawImage(this.backgroundImage,
-          i * (this.backgroundImage.width) + this.origin,
-          j * (this.backgroundImage.height) + this.origin);
-      }
-    }
-  }
-
   update() {
     this.ctx.clearRect(-1, -1, this.canvasElement.clientWidth + 1, this.canvasElement.clientHeight + 1);
-    this.drawBackground();
     for (let i = 0; i < this.objects_on_canvas.length; i++) {
       this.objects_on_canvas[i].draw();
     }
